7fa143525a015c59df2950324f30bd5b06dae030
lionelsu <lionel_suicune@hotmail.com>
2023-04-13 14:34:54 -0400

    req 01: Added sample class routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './services/pages/Login';
+import Search from './services/pages/Search';
+import Album from './services/pages/Album';
+import Favorites from './services/pages/Favorites';
+import Profile from './services/pages/Profile';
+import ProfileEdit from './services/pages/ProfileEdit';
+import NotFound from './services/pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+
+        <BrowserRouter>
+          <Route path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route component={ NotFound } />
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
