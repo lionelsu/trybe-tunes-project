b88745a8dbceb3c17d144479b12e938661bada3c
lionelsu <lionel_suicune@hotmail.com>
2023-04-13 18:29:13 -0400

    req 02: implemented a form validation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Login from './services/pages/Login';
-import Search from './services/pages/Search';
-import Album from './services/pages/Album';
-import Favorites from './services/pages/Favorites';
-import Profile from './services/pages/Profile';
-import ProfileEdit from './services/pages/ProfileEdit';
-import NotFound from './services/pages/NotFound';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
---
+++ src/pages/Login.jsx
@@ -0,0 +1,66 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+
+class Login extends React.Component {
+  state = {
+    name: '',
+    loading: false,
+    nameValid: false,
+  };
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    const maxLength = 3;
+    const nameValid = value.length >= maxLength;
+    this.setState({
+      name: value,
+      nameValid,
+    });
+  };
+
+  creator = async () => {
+    const { name } = this.state;
+    const { history } = this.props;
+
+    this.setState({ loading: true });
+    await createUser({ name });
+    history.push('/search');
+    this.setState({ loading: false });
+  };
+
+  render() {
+    const { nameValid, loading } = this.state;
+    return (
+      <div data-testid="page-login">
+        {loading ? (<Loading />) : (
+          <form>
+            <input
+              type="text"
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="submit"
+              disabled={ !nameValid }
+              onClick={ this.creator }
+              data-testid="login-submit-button"
+            >
+              Entrar
+            </button>
+          </form>
+        )}
+
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default Login;
