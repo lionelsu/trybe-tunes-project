456073e4c4196dbc4ab16c0a9e4d4eb6cd3fa298
lionelsu <lionel_suicune@hotmail.com>
2023-04-14 18:20:23 -0400

    req 06: sinceramente eu não estou entendendo mais nada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -11,15 +11,17 @@ import NotFound from './pages/NotFound';
 class App extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/search" component={ Search } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <Route exact component={ NotFound } />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact component={ NotFound } />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   state = {
     name: '',
+    loading: false,
     nameValid: false,
+    artistName: '',
+    albums: null,
   };
 
   changer = (event) => {
@@ -17,27 +23,67 @@ class Search extends React.Component {
     });
   };
 
+  creator = async () => {
+    const { name } = this.state;
+    this.setState({ loading: true });
+    const albums = await searchAlbumsAPI(name);
+    this.setState({
+      name: '',
+      loading: false,
+      albums,
+      artistName: name,
+    });
+  };
+
   render() {
-    const { name, nameValid } = this.state;
+    const { name, nameValid, loading, artistName, albums } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            name="name"
-            value={ name }
-            onChange={ this.changer }
-          />
-          <button
-            type="submit"
-            disabled={ !nameValid }
-            data-testid="search-artist-button"
-          >
-            Pesquisar
-          </button>
-        </form>
+        {loading ? (<Loading />) : (
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              name="name"
+              value={ name }
+              onChange={ this.changer }
+            />
+            <button
+              type="submit"
+              disabled={ !nameValid }
+              onClick={ this.creator }
+              data-testid="search-artist-button"
+            >
+              Pesquisar
+            </button>
+          </form>
+        )}
+
+        {albums && (
+          <div>
+            <p>
+              {`Resultado de álbuns de: ${artistName}`}
+            </p>
+            {albums.length > 0 ? (
+              <ul>
+                {albums.map((album) => (
+                  <li key={ album.collectionId }>
+                    <div>{album.collectionName}</div>
+                    <Link
+                      to={ `/album/${album.collectionId}` }
+                      data-testid={ `link-to-album-${album.collectionId}` }
+                    >
+                      Ver mais
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>Nenhum álbum foi encontrado</p>
+            )}
+          </div>
+        )}
       </div>
     );
   }
