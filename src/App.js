aaa20339de672de093f0f204b66f1351942c06f7
lionelsu <lionel_suicune@hotmail.com>
2023-04-14 00:58:17 -0400

    fix: broken routes after req 05

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,25 +7,19 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
-import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <p>TrybeTunes</p>
-
-        <BrowserRouter>
-          <Header />
-          <Route path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route component={ NotFound } />
-        </BrowserRouter>
-      </div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact component={ NotFound } />
+      </Switch>
     );
   }
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    name: '',
+    nameValid: false,
+  };
+
+  changer = (event) => {
+    const { value } = event.target;
+    const maxLength = 2;
+    const nameValid = value.length >= maxLength;
+    this.setState({
+      name: value,
+      nameValid,
+    });
+  };
+
   render() {
+    const { name, nameValid } = this.state;
     return (
       <div data-testid="page-search">
-        {}
+        <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="name"
+            value={ name }
+            onChange={ this.changer }
+          />
+          <button
+            type="submit"
+            disabled={ !nameValid }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
