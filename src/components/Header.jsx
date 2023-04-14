4c01e4312043581d32ce1505351091b7eea21e88
lionelsu <lionel_suicune@hotmail.com>
2023-04-13 20:54:13 -0400

    req 04: Created nav links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -12,19 +13,40 @@ class Header extends React.Component {
     this.getUserName();
   }
 
-  async getUserName() {
+  getUserName = async () => {
     this.setState({ loading: true });
     const { name } = await getUser();
     this.setState({ name, loading: false });
-  }
+  };
 
   render() {
     const { name, loading } = this.state;
     return (
       <header data-testid="header-component">
-        {loading ? (<Loading />) : (
-          <p data-testid="header-user-name">{ name }</p>
-        )}
+        {
+          loading
+            ? (<Loading />)
+            : (<p data-testid="header-user-name">{ name }</p>)
+        }
+        <nav>
+          <ul>
+            <li>
+              <Link to="/search" data-testid="link-to-search">
+                Search
+              </Link>
+            </li>
+            <li>
+              <Link to="/favorites" data-testid="link-to-favorites">
+                Favorites
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile" data-testid="link-to-profile">
+                Profile
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
